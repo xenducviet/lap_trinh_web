c651a38ff6188a7db8548699fe13741dfa7bc622
xenducviet <ducvietxmhg@gmail.com>
2023-10-09 01:05:22 +0700

    Add project files.

---
+++ lap_trinh_web/api.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace wtbc
+{
+    public partial class api : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                double A = double.Parse(Request["a"]);
+                double B = double.Parse(Request["b"]);
+                double C = double.Parse(Request["c"]);
+                double D = double.Parse(Request["d"]);
+
+                double kq = (A + B + C + D) / 3.0;
+
+                Response.Write(kq.ToString());
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ lap_trinh_web/mycode.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $("#nut").click(function () {
+        var data = {
+            a: $('#s1').val(),
+            b: $('#s2').val(),
+            c: $('#s3').val(),
+            d: $('#s4').val(),
+
+        }
+        $.post("api.aspx",
+            data,
+            function (res) {
+                $('#ketqua').html(res);
+            });
+    });
+});
